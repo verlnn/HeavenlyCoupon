fa5d4db985a41805c208ae9f6b3e475156307144
verlnn <mlnsukim2327@gmail.com>
2026-01-12 22:16:50 +0900

    first commit

---
+++ src/main/java/io/hc/heavenlycoupon/HeavenlyCouponApplication.java
@@ -0,0 +1,13 @@
+package io.hc.heavenlycoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavenlyCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeavenlyCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hc/heavenlycoupon/HeavenlyCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hc.heavenlycoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavenlyCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
